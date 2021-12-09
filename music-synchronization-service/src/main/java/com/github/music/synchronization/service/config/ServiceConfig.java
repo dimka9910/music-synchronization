28e2b9f7bb6e2479d559d389e46c70de8cb3ace1
Neupokoev.D <Neupokoev.D@digdes.com>
2021-12-09 17:39:02 +0300

    add: RH-3 users entity, repo, liquibase

--- music-synchronization-service/src/main/java/com/github/music/synchronization/service/config/ServiceConfig.java
+++ music-synchronization-service/src/main/java/com/github/music/synchronization/service/config/ServiceConfig.java
@@ -1,10 +1,16 @@
 package com.github.music.synchronization.service.config;
 
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.github.music.synchronization.service.db.entity")
+@EnableJpaRepositories(basePackages = "com.github.music.synchronization.service.db.repository")
 @ComponentScan(basePackages = "com.github.music.synchronization.service")
 public class ServiceConfig {
 
